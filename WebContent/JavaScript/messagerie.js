e8ded17f8ca7f8d70a885ae9de9b8b3dcf4abb75
Aurelien Harle <aurelienharle@hotmail.com>
2016-11-17 10:41:32 +0100

    Modification messagerie ajout logo loupe reparation du menu

--- WebContent/JavaScript/messagerie.js
+++ WebContent/JavaScript/messagerie.js
@@ -1,3 +1,11 @@
 /**
- * 
+ * Contain every function linked to messagerie
+ * changeVisibility() Switch visibility of search bar and search logo
  */
+
+function changeVisibility(element){
+	
+	element.setAttribute('style','visibility: hidden;');
+	
+	
+}
