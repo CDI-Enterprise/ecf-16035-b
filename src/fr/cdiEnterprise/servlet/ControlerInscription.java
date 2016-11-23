e6963f23968213f43c3539ff01d97e7482f2a86e
togouna <soulnango@yahoo.fr>
2016-11-23 02:11:14 +0100

    maj

--- src/fr/cdiEnterprise/servlet/ControlerInscription.java
+++ src/fr/cdiEnterprise/servlet/ControlerInscription.java
@@ -2,7 +2,6 @@ package fr.cdiEnterprise.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,10 +12,9 @@ import fr.cdiEnterprise.dao.AccesBaseInscrit;
 import fr.cdiEnterprise.model.Inscription;
 
 /**
- * Servlet implementation class ControlerInscription
+ * Servlet implementation class ControlerInscriptions
  */
-@WebServlet(
-		name="ControlerInscription", urlPatterns={"/inscription"})
+@WebServlet(name="ControlerInscription", urlPatterns={"/inscription"})
 public class ControlerInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -42,9 +40,10 @@ public class ControlerInscription extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("post");
 		
+		
 		// Récupération des variables depuis le formulaire Inscription.jsp
 		// récupération possible car adresse indiquée dans le formulaire Inscription.jsp
-		int reference = 0;
+		int reference = 4;
 		String statut = request.getParameter("radio");
 		String nom = request.getParameter("nom");
 		String prenom = request.getParameter("prenom");
@@ -54,19 +53,20 @@ public class ControlerInscription extends HttpServlet {
 		
 		// Instanciation d' un objet de type Inscription
 		Inscription exemple = new Inscription(reference, statut, nom, prenom, email, motDePasse, confirmation);
-		
+		//Inscription ex = new Inscription(reference, statut, nom, prenom, email, motDePasse, confirmation);
 		// Instanciation d' un objet de type AccesBaseInscrit
 		AccesBaseInscrit base = new AccesBaseInscrit();
-		
+		//AccesBaseInscrit con = new AccesBaseInscrit();
+		//con.inscription(ex);
 		// Application de la méthode inscription de AccesBaseInscrit en lui donnant en paramètre l' objet exemple
 		base.inscription(exemple);
 		
-		// essai d' envoi de la réponse vers une page html.
-		response.sendRedirect("../InscriptionReussie.html");
+		// TODO essai d' envoi de la réponse vers une page html.
+	//	response.sendRedirect("../InscriptionReussie.html");
 		
-		// essai pour générer un récapitulatif de l' inscription.
-		request.setAttribute("inscription", exemple);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("../Recapitulatif.jsp");
+		// TODO essai pour générer un récapitulatif de l' inscription.
+	//	request.setAttribute("inscription", exemple);
+	//	RequestDispatcher dispatcher = request.getRequestDispatcher("../Recapitulatif.jsp");
 		
 		
 		
